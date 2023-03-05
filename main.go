afdc19a0a9198693a7b96b4af65a7d48f687d5e0
shiva <kamarapushivachandra@gmail.com>
2023-03-05 21:04:24 +0530

    mry pool

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	ty "shiva/types"
+)
+
+func populate() *ty.Menu {
+	return &ty.Menu{
+		Id:    "file",
+		Value: "File",
+		Popup: ty.Popup{
+			MenuItem: []ty.MenuItem{
+				{Value: "New", OnClick: "CreateDoc()"},
+				{Value: "Open", OnClick: "OpenDoc()"},
+				{Value: "Save", OnClick: "SaveDoc()"},
+			},
+		},
+	}
+}
 func main() {
 
 }
--- main_test.go
+++ main_test.go
@@ -3,8 +3,76 @@ package main
 import (
 	"encoding/json"
 	ty "shiva/types"
+	"sync"
 	"testing"
 )
+
+var pool = sync.Pool{
+	New: func() any {
+		return &ty.Menu{}
+	},
+}
+
+func BenchmarkJsonUnMarshallerWOPool1(b *testing.B) {
+	data := []byte(`{
+        "menu": {
+            "id": "file",
+            "value": "File",
+            "popup": {
+                "menuitem": [
+                    {"value": "New", "onclick": "CreateDoc()"},
+                    {"value": "Open", "onclick": "OpenDoc()"},
+                    {"value": "Save", "onclick": "SaveDoc()"}
+                ]
+            }
+        }
+    }`)
+	menu := &ty.Menu{}
+	for i := 0; i < b.N; i++ {
+		_ = json.Unmarshal(data, &menu)
+	}
+}
+func BenchmarkJsonUnMarshallerWOPool(b *testing.B) {
+	data := []byte(`{
+        "menu": {
+            "id": "file",
+            "value": "File",
+            "popup": {
+                "menuitem": [
+                    {"value": "New", "onclick": "CreateDoc()"},
+                    {"value": "Open", "onclick": "OpenDoc()"},
+                    {"value": "Save", "onclick": "SaveDoc()"}
+                ]
+            }
+        }
+    }`)
+	for i := 0; i < b.N; i++ {
+		menu := &ty.Menu{}
+		_ = json.Unmarshal(data, &menu)
+	}
+}
+
+func BenchmarkJsonUnMarshallerPool(b *testing.B) {
+	m := pool.Get().(*ty.Menu)
+	defer pool.Put(m)
+	data := []byte(`{
+        "menu": {
+            "id": "file",
+            "value": "File",
+            "popup": {
+                "menuitem": [
+                    {"value": "New", "onclick": "CreateDoc()"},
+                    {"value": "Open", "onclick": "OpenDoc()"},
+                    {"value": "Save", "onclick": "SaveDoc()"}
+                ]
+            }
+        }
+    }`)
+	for i := 0; i < b.N; i++ {
+		_ = json.Unmarshal(data, m)
+	}
+
+}
 
 func BenchmarkJsonMarshaller(b *testing.B) {
 	menu := ty.Menu{
